ef08199dbd39fe1d6f6af14d315b46875f476beb
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-06 21:57:13 +0200

    Exercise 1..8. Write a program to count blanks, tabs, and newline

--- main.c
+++ main.c
@@ -12,15 +12,25 @@
  */
 
 #include <stdio.h>
-/*1.5.3 count lines in input */
+/* Exercise 1..8. Write a program to count blanks, tabs, and newline */
 int main() {
-    int c, nl;
+    int c, nl, nt, ns;
     
     nl = 0;
-    while ((c = getchar()) != EOF) 
-        if (c == '\n')
-            ++nl;
-    printf("%d\n", nl);
+    nt = 0;
+    ns = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            ++nl;                    
+        }
+        if (c == '\t') {
+            ++nt;
+        }
+        if (c == ' ') {
+            ++ns;
+        }
+    }
+    printf("%dlines\n%dtabs\n%dspaces\n", nl, nt, ns);
     return 0;
 }
 
