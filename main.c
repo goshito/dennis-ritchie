b507cd74c698d81ced20c840661219a9b76c9fa1
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-06 20:52:11 +0200

    count characters in input; 1st version

--- main.c
+++ main.c
@@ -12,10 +12,14 @@
  */
 
 #include <stdio.h>
-/* Exercise 1-7. Write a program to print the value of EOF */
-int main() {
-    printf("%d", EOF);
-    return 0;
+/* count characters in input; 1st version  */
+main() {
+    long nc;
+    
+    nc = 0;
+    while (getchar() != EOF)
+        ++nc;
+    printf("%ld\n", nc);
 }
 
 
