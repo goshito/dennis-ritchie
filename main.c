e9223c903efb6af043aadf2d97f4251d7e7be2a2
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-13 12:40:06 +0200

    1.2 Exercise

--- main.c
+++ main.c
@@ -1,40 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: georgi
- *
- * Created on February 6, 2016, 12:45 AM
- */
+/*The C Programming Language, 2nd Edition, by Kernighan and Ritchie
+Exercise 1.02 on page 8*/
 
 #include <stdio.h>
-//why it doesn't recognize digits
-// count digits, white space, others
 
-int main() {
-    int c, i, nwhite, nother;
-    int ndigit[10];
-    
-    nwhite = nother = 0;
-    for (i = 0; i < 10; i++)
-        ndigit[i] = 0;
-    
-    while ((c = getchar() != EOF))
-            if (c >= '0' && c <= '9')
-                ++ndigit[c-'0'];
-            else if (c == ' ' || c == '\n' || c == '\t')
-                ++nwhite;
-            else
-                ++nother;
-            
-    printf("digits =");
-    for (i = 0; i < 10; ++i)
-        printf(" %d", ndigit[i]);
-    printf(", whitespace = %d, other = %d\n", 
-            nwhite, nother);          
-    
+int main(void) {
+    printf("Audible or visual alert. \a\n");
+    printf("Form feed. \f\n");
+    printf("This escape, \r, moves the active position to the initial position of the current line.\n");
+    printf("Vertical tab \v is tricky, as its behaviour is unspecified under certain conditions.\n");
+    return 0;
 }
