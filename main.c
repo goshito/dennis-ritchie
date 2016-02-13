5744ec5e73c4a90fbc1d61bd14b20195ab70662c
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-13 17:27:02 +0200

    Exercise 1.07 on page 17
    Write a program to print the value of EOF .

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-int main(void)
-{
-  printf("Press a key. ENTER would be nice :-)\n\n");
-  printf("The expression getchar() != EOF evaluates to %d\n", getchar() != EOF);
-  return 0;
+int main() {
+    printf("%d", EOF);
 }
