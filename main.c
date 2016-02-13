816a5c1926712065f6b43f495fc1749c99713ae3
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-13 13:44:39 +0200

    copy input to output 1st ver

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
+// copy input to outpu first version
 int main() {
-    float fahr, celsius;
-    int lower, upper, step;
+    int c;
     
-    lower = 0;
-    upper = 300;
-    step = 20;
-    
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
     }
-    return 0;
 }
